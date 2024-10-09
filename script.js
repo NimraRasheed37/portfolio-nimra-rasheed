f670b8212e9e856f665c5fa986fe37743b8a01ad
Nimra Rasheed <60661441+NimraRasheed37@users.noreply.github.com>
2024-10-09 10:02:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+//Function to control Sidebar
+document.getElementById('nav-bar').addEventListener('click', function() {
+    const sidebar = document.getElementById('side-bar');
+    // Toggle the "active" class to show or hide the sidebar
+    sidebar.classList.toggle('active');
+});
