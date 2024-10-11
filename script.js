7f3f9d954fe0aba5a4dd4be69dee325905a1bc76
NimraRasheed37 <binteshaheen.na@gmail.com>
2024-10-11 17:33:17 +0500

    contact form updated

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const navBar = document.getElementById('nav-bar');
 const sideBar = document.getElementById('side-bar');
 const sideBarLinks = document.querySelectorAll("#side-bar .side-bar-links a");
 
-//open sidebarhwn clicked on navBar 
+
+//open sidebar when clicked on navBar 
 navBar.addEventListener('click', () => {
     sideBar.classList.toggle("active");
 });
@@ -13,4 +14,13 @@ sideBarLinks.forEach(link => {
         sideBar.classList.remove("active");
     });
 });
- 
+
+
+const msgSent = document.getElementById('msg-sent');
+const msgBtn = document.getElementById('msg-btn');
+//show msgSent when msgBtn is clicked
+msgBtn.addEventListener('click', (event) => {
+    event.preventDefault(); //prevent page reloading
+    msgSent.classList.toggle("active");
+});
+
